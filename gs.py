722ca654a2e5ad66b7277d1919160ccacd76e523
Josué Faria <josufaria18@gmail.com>
2025-11-13 14:06:20 -0300

    Adicionando os comentarios

--- gs.py
+++ gs.py
@@ -5,13 +5,14 @@
 # - Nome: Julia Schiavi – RM: 562418
 # =====================================================================
 
+# Função que avalia o risco de burnout com base em estresse, sono e atividade física
 def avaliar_bem_estar(estresse, sono, atividade):
-
+# Verifica alto risco: estresse alto e pouco sono
     if estresse >= 7 and sono < 6:
         return "ALTO"
-
+# Verifica risco médio: estresse moderado ou sono insuficiente
     elif estresse >= 5 or sono < 7:
         return "MÉDIO"
-    
+# Caso contrário, risco baixo    
     else:
         return "BAIXO"
